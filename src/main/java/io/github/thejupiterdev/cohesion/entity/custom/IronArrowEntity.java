64d22c8f5623444f157a9fb49c41ecd0a36d832c
TheJupiterDev <thatwierdodude@gmail.com>
2025-06-20 15:24:24 -0500

    ✨ More Arrows (Resources)

--- src/main/java/io/github/thejupiterdev/cohesion/entity/custom/IronArrowEntity.java
+++ src/main/java/io/github/thejupiterdev/cohesion/entity/custom/IronArrowEntity.java
@@ -1,18 +1,22 @@
 package io.github.thejupiterdev.cohesion.entity.custom;
 
-import io.github.thejupiterdev.cohesion.entity.ModEntities;
 import io.github.thejupiterdev.cohesion.item.ModItems;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
 public class IronArrowEntity extends ArrowEntity {
     public IronArrowEntity(EntityType<? extends ArrowEntity> type, World world) {
         super(type, world);
+        this.setDamage(3);
+    }
+
+    public IronArrowEntity(EntityType<? extends ArrowEntity> type, World world, double x, double y, double z) {
+        super(type, world);
+        this.setPosition(x, y, z);
         this.setDamage(3);
     }
 
